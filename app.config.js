8e01303fcb70ab0040bb72eb10ad11eaa83a3da2
uzayrgaffar <uzayrgaffar@gmail.com>
2025-03-13 16:52:22 +0000

    production

--- app.config.js
+++ app.config.js
@@ -1,41 +1,39 @@
 export default {
-  "owner": "uzayrgaffar",
-  "expo": {
-    "name": "QadhaApp",
-    "slug": "QadhaApp",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "userInterfaceStyle": "light",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+  owner: "uzayrgaffar",
+  expo: {
+    name: "QadhaApp",
+    slug: "QadhaApp",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
     },
-    "plugins": [
+    plugins: [
       "@react-native-google-signin/google-signin",
-      "expo-build-properties", 
+      "expo-build-properties"
     ],
-    "ios": {
-      "supportsTablet": true,
-      "bundleIdentifier": "com.uzayrgaffar.qadhaapp",
-      "googleServicesFile": process.env.GOOGLE_SERVICES_INFOPLIST
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.uzayrgaffar.qadhaapp"
     },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
       },
-      "package": "com.uzayrgaffar.qadhaapp",
-      "googleServicesFile": process.env.GOOGLE_SERVICES_JSON
+      package: "com.uzayrgaffar.qadhaapp"
     },
-    "web": {
-      "favicon": "./assets/favicon.png"
+    web: {
+      favicon: "./assets/favicon.png"
     },
-    "extra": {
-      "eas": {
-        "projectId": "7de41553-636c-42ad-b26d-7027d42cf51b"
+    extra: {
+      eas: {
+        projectId: "7de41553-636c-42ad-b26d-7027d42cf51b"
       }
     }
   }
-}
+};
