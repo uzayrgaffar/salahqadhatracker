703438babcdb3067e162285bf4110277777b3439
uzayrgaffar <uzayrgaffar@gmail.com>
2025-03-14 03:52:33 +0000

    doctor fixes

--- app.config.js
+++ app.config.js
@@ -4,31 +4,13 @@ export default {
     name: "QadhaApp",
     slug: "QadhaApp",
     version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/icon.png",
     userInterfaceStyle: "light",
-    splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff"
-    },
-    // plugins: [
-    //   "@react-native-google-signin/google-signin",
-    //   "expo-build-properties"
-    // ],
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.uzayrgaffar.qadhaapp"
     },
     android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#ffffff"
-      },
       package: "com.uzayrgaffar.qadhaapp"
-    },
-    web: {
-      favicon: "./assets/favicon.png"
     },
     extra: {
       eas: {
