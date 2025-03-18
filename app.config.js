f77a4241794e3280e6edbc49ede3539a046eef17
uzayrgaffar <uzayrgaffar@gmail.com>
2025-03-18 14:58:41 +0000

    buildNumber

--- app.config.js
+++ app.config.js
@@ -8,7 +8,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.uzayrgaffar.qadhaapp",
-      buildNumber: "2",
+      buildNumber: "3",
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false
       }
