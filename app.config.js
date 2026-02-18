9274cd5b90267da7d153ff382d330150fc33f1e4
uzayrgaffar <uzayrgaffar@gmail.com>
2026-02-18 00:55:02 +0000

    1.0.4

--- app.config.js
+++ app.config.js
@@ -8,7 +8,7 @@ export default {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#5CB390"
     },
-    version: "1.1.0",
+    version: "1.0.4",
     userInterfaceStyle: "light",
     ios: {
       jsEngine: "hermes",
